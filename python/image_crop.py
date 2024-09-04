49a1e262b232c4af5b1f7f50af9275bea89b0a90
Trần Hoàn <daibangcam@gmail.com>
2024-09-04 10:44:53 +0700

    Create image_crop.py

---
+++ python/image_crop.py
@@ -0,0 +1,23 @@
+﻿# import thư viện OpenCV
+import cv2
+  
+# gán file ảnh 01.jpg vào biến img
+img = cv2.imread('01.jpg')
+  
+# đặt tên cửa sổ hiển thị file ảnh 
+# cv2.WINDOW_AUTOSIZE -> autosize window theo ảnh
+# cv2.WINDOW_NORMAL -> fit ảnh trong window
+cv2.namedWindow("Original RGB", cv2.WINDOW_NORMAL)
+
+# crop ảnh từ tọa độ (50,60) đến (350,360)
+roi = img[50:350, 60:360]
+  
+# hiển thị file ảnh
+cv2.imshow('Original RGB', img)
+cv2.imshow('Rotate image', roi)
+
+# đợi người dùng nhấn phím bất kì
+cv2.waitKey(0)
+  
+# xóa toàn bộ cửa sổ đã tạo ra
+cv2.destroyAllWindows()
