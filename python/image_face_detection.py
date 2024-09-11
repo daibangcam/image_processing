d77963a569d00b5d310f1ff06cb8d1c2d5aa0c83
Trần Hoàn <daibangcam@gmail.com>
2024-09-11 16:58:49 +0700

    Update image_face_detection.py

--- python/image_face_detection.py
+++ python/image_face_detection.py
@@ -2,10 +2,10 @@
 import cv2
 
 # load thư viện nhận diện khuôn mặt vào biến haar_cascade (đặt tên biến tùy ý)
-haar_cascade = cv2.CascadeClassifier('Haarcascade_frontalface_default.xml')
+haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
   
 # load ảnh
-img = cv2.imread('01.jpg')
+img = cv2.imread('01.png')
   
 # chuyển sang ảnh xám
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
