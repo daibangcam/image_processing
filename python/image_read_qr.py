a15b47bc85f48ca608643023f12dba7ed4713dd5
Trần Hoàn <daibangcam@gmail.com>
2024-09-17 12:41:07 +0700

    Create image_read_qr.py

---
+++ python/image_read_qr.py
@@ -0,0 +1,22 @@
+﻿# import thư viện
+import cv2
+import qrcode
+
+# đọc file ảnh chứa qr code
+img = cv2.imread("qr_code.png")
+
+# show code qr
+cv2.imshow("QR Code", img)
+
+# gọi bộ giải mã qr code
+detector=cv2.QRCodeDetector()
+
+# giải mã qr code
+val,b,c=detector.detectAndDecode(img)
+
+# in ra nội dung qr code
+print(val)
+
+# chờ nhấn phím rồi thoát
+cv2.waitKey(0)
+cv2.destroyAllWindows()
